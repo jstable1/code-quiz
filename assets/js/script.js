dee341a446280962668bf61a50eb945a3a299c52
jstable1 <jessica.stabler@gmail.com>
2022-04-09 21:40:11 -0600

    created questions variable

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var questions = [{
+    question: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    correctAnswer: 2
+}, {
+    question: 'The condition in an if/else statement is enclosed with _____',
+    choices: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+    correctAnswer: 2
+}, {
+    question: 'Arrays in JavaScript can be used to store _____',
+    choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+    correctAnswer: 3
+}, {
+    question: 'String values must be enclosed within _____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+    correctAnswer: 2
+}, {
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['Javascript', 'terminal/bash', 'for loops', 'console.log'],
+    correctAnswer: 3
+}]
