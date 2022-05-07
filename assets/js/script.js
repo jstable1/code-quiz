5c14c61b08f76c3473a5596afa656f771f559295
jstable1 <jessica.stabler@gmail.com>
2022-05-06 20:15:52 -0600

    created functions to start the quiz and to display the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+const startQuizBtn = document.getElementById("start-quiz");
+// declare a global variable to hold current question index - starts off with 0
+let currentQuestion = 0;
+const questionContainer = document.getElementById("questionContainer");
+
+startQuizBtn.addEventListener("click", e => startQuiz(e));
+
 var questions = [{
     question: 'Commonly used data types DO NOT include:',
     choices: ['strings', 'booleans', 'alerts', 'numbers'],
@@ -18,4 +25,41 @@ var questions = [{
     question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
     choices: ['Javascript', 'terminal/bash', 'for loops', 'console.log'],
     correctAnswer: 3
-}]
+}];
+
+var startQuiz = function (e) {
+    //prevents the browswer from refreshing
+    e.preventDefault()
+    document.getElementById("startContainer").setAttribute("class", "hidden");
+    document.getElementById("questionContainer").setAttribute("class", "quizSection");
+    displayNextQuestion()
+}
+
+var displayNextQuestion = function () {
+    var activeQuestion = questions[currentQuestion];
+    questionContainer.innerHTML = "";
+    var questionEL = document.createElement("p");
+    questionEL.innerText = activeQuestion.question;
+    questionContainer.appendChild(questionEL);
+    for (let i = 0; i < activeQuestion.choices.length; i++) {
+        var answersEl = document.createElement("button");
+        answersEl.innerText = activeQuestion.choices[i];
+        questionContainer.appendChild(answersEl);
+        answersEl.addEventListener("click", function (event) {
+            event.preventDefault();
+            // checkAnswer()
+            currentQuestion++
+            displayNextQuestion()
+        })
+    }
+}
+
+
+var checkAnswer
+
+var endQuiz
+
+var saveInitials
+
+var timer
+
